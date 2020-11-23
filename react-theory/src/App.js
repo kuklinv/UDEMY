5fff7d8bbf6e4eca564e7dc1faa27ba82f820dfe
Vitaly <vitalykuklin@gmail.com>
2020-11-24 00:56:37 +0700

    Feature: add first state without for_of/ working version[ref:no-ref]

--- react-theory/src/App.js
+++ react-theory/src/App.js
@@ -9,22 +9,57 @@ class App extends Component {
     const rootDivStyle = {
       textAlign: "center",
     };
+    this.state = {
+      pics: [
+        { name: "first pic", id_pic: 1 },
+        { name: "second pic", id_pic: 2 },
+        { name: "third pic", id_pic: 3 },
+      ],
+      pageTitle: "Hello UDEMY React course",
+    };
+
+    const pics = this.state.pics;
 
     return (
       <div style={rootDivStyle}>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 style={{ color: "yellow" }}>Hello UDEMY React course</h1>
+          <h1 style={{ color: "yellow" }}>{this.state.pageTitle}</h1>
           <p style={{ color: "gray" }}>start at 11/22/2020</p>
           <span></span>
-          <Picture name={"first pic"} id_pic={1} />
-          <Picture name={"second pic"} id_pic={2}>
-            <span style={{ color: "red" }}>children pic</span>
-          </Picture>
+          <Picture name={pics[0].name} id_pic={pics[0].id_pic} />
+          <Picture name={pics[1].name} id_pic={pics[1].id_pic} />
+          <Picture name={pics[2].name} id_pic={pics[2].id_pic} />
           <span></span>
-          <SimpleComponent />
         </header>
       </div>
+
+      // <div style={rootDivStyle}>
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <h1 style={{ color: "yellow" }}>{this.state.pageTitle}</h1>
+      //     <p style={{ color: "gray" }}>start at 11/22/2020</p>
+      //     <span></span>
+      //     <Picture name={pics[0].name} id_pic={pics[0].id_pic} />
+      //     <Picture name={pics[1].name} id_pic={pics[1].id_pic} />
+      //     <Picture name={pics[2].name} id_pic={pics[2].id_pic} />
+      //   </header>
+      // </div>
+
+      // <div style={rootDivStyle}>
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <h1 style={{ color: "yellow" }}>Hello UDEMY React course</h1>
+      //     <p style={{ color: "gray" }}>start at 11/22/2020</p>
+      //     <span></span>
+      //     <Picture name={"first pic"} id_pic={1} />
+      //     <Picture name={"second pic"} id_pic={2}>
+      //       <span style={{ color: "red" }}>children pic</span>
+      //     </Picture>
+      //     <span></span>
+      //     <SimpleComponent />
+      //   </header>
+      // </div>
 
       //   <div className="App">
       //     <header className="App-header">
