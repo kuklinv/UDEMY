2a1de67bfa6c64e77a0f31ec7ba5d4098e4e0b01
Vitaly <vitalykuklin@gmail.com>
2020-11-29 13:57:52 +0700

    Feature: add dinamic classes to component style (with scc)[ref:no-ref]

--- react-theory/src/PictureComponent/Picture.js
+++ react-theory/src/PictureComponent/Picture.js
@@ -4,13 +4,20 @@ import "./Picture.css";
 class Picture extends Component {
   // super({props})
   render(props) {
+    const inputClasses = ["input"];
+
+    if (this.props.name !== "") {
+      inputClasses.push("green");
+    } else {
+      inputClasses.push("red");
+    }
+
+    if (this.props.name.length > 4) {
+      inputClasses.push("bold");
+    }
+
     return (
-      <div
-        style={{
-          border: "1px solid #ccc",
-          marginBottom: "10px",
-        }}
-      >
+      <div className="basePic">
         <div className="pic">
           <h4>name: {this.props.name}</h4>
           <p>id: {this.props.id_pic}</p>
@@ -19,6 +26,7 @@ class Picture extends Component {
             type="text"
             onChange={this.props.onChangeName}
             value={this.props.name}
+            className={inputClasses.join(" ")}
           />
           {/* <button onClick={this.props.changeTitleButton}>click</button> */}
           <button onClick={this.props.onDelete}>delete</button>
