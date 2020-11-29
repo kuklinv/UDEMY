ea1066ee050cf83dec96a5cc74b05bc1daee78e2
Vitaly <vitalykuklin@gmail.com>
2020-11-29 22:44:25 +0700

    Feature: use classes with module.scss [ref:no-ref]

--- react-use-sass/src/App.js
+++ react-use-sass/src/App.js
@@ -1,16 +1,16 @@
 import logo from "./logo.svg";
-import "./App.scss";
+import classes from "./App.module.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="classes.App">
+      <header className={classes.AppHeader}>
+        <img src={logo} className={classes["App-logo"]} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className={classes["App-link"]}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
