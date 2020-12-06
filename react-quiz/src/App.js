10f147c077ebf76ce0e28b8b83f16dcd2a848da4
kuklinv <vitalykuklin@gmail.com>
2020-12-06 10:43:58 +0700

    Feature: prepare Layout root component and Laout.css module[ref:no-ref]

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -1,11 +1,16 @@
-import './App.css';
+import React, {Component} from 'react';
+import Layout from './hoc/Layout/Layout';
 
-function App() {
-    return (
-        <div className="App">
-
-        </div>
-    );
+class App extends Component {
+    render() {
+        return (
+            <Layout>
+                <div style={{width: 400, border: '2px solid black'}}>
+                    <h2>Layout</h2>
+                </div>
+            </Layout>
+        )
+    }
 }
 
 export default App;
