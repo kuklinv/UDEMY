aba8c8ed8fef79ae43ef962c3bf713f2a7c5ebf2
Vitaly <vitalykuklin@gmail.com>
2020-12-16 00:21:11 +0700

    Feature: change headers [ref:#14]

--- react-quiz/src/containers/Quiz/Ouiz.js
+++ react-quiz/src/containers/Quiz/Ouiz.js
@@ -1,130 +1,124 @@
-import React, {Component} from 'react';
-import classes from './Quiz.module.css';
+import React, { Component } from "react";
+import classes from "./Quiz.module.css";
 import ActivQuiz from "../../components/ActivQuiz/ActivQuiz";
 import FinishedQuiz from "../../components/FinishedQuiz/FinishedQuiz";
 
 class Quiz extends Component {
-    state = {
-        rezults: {}, // {[key]: success || error}
-        quizFinished: false,
-        activQuestion: 0,
-        answerState: null,
-        quiz: [
-            {
-                questionId: 1,
-                question: 'what color is the sky?',
-                answers: [
-                    {text: 'Red', id: 1},
-                    {text: 'Yellow', id: 2},
-                    {text: 'Blue', id: 3},
-                    {text: 'Black', id: 4},
-                ],
-                wrightAnswerId: 3
-            },
-            {
-                questionId: 2,
-                question: 'what year is it?',
-                answers: [
-                    {text: '2020', id: 1},
-                    {text: '1972', id: 2},
-                    {text: '2024', id: 3},
-                    {text: '1812', id: 4},
-                ],
-                wrightAnswerId: 1
-            }
-        ]
-    };
+  state = {
+    rezults: {}, // {[key]: success || error}
+    quizFinished: false,
+    activQuestion: 0,
+    answerState: null,
+    activeHeader: "Welcome to Quiz app",
+    startFinishedHeader: "Answer all questions",
+    quiz: [
+      {
+        questionId: 1,
+        question: "what color is the sky?",
+        answers: [
+          { text: "Red", id: 1 },
+          { text: "Yellow", id: 2 },
+          { text: "Blue", id: 3 },
+          { text: "Black", id: 4 },
+        ],
+        wrightAnswerId: 3,
+      },
+      {
+        questionId: 2,
+        question: "what year is it?",
+        answers: [
+          { text: "2020", id: 1 },
+          { text: "1972", id: 2 },
+          { text: "2024", id: 3 },
+          { text: "1812", id: 4 },
+        ],
+        wrightAnswerId: 1,
+      },
+    ],
+  };
 
-    chengToNextQuestion = () => {
-        this.setState({
-            activQuestion: this.state.activQuestion + 1,
-            answerState: null
-        })
+  chengToNextQuestion = () => {
+    this.setState({
+      activQuestion: this.state.activQuestion + 1,
+      answerState: null,
+    });
+  };
+
+  quizFinished = () => {
+    return this.state.activQuestion + 1 === this.state.quiz.length;
+  };
+
+  selectQuizAnswerHandler = (answerId) => {
+    // wait change quiz when we have a right answer, for not finished quiz with double click wright button
+    if (this.state.answerState) {
+      const key = Object.keys(this.state.answerState)[0];
+      if (this.state.answerState[key] === "success") {
+        return;
+      }
     }
 
-    quizFinished = () => {
-        return this.state.activQuestion + 1 === this.state.quiz.length;
-    }
+    console.log("you select:", answerId);
 
-    selectQuizAnswerHandler = (answerId) => {
+    const qwestion = this.state.quiz[this.state.activQuestion];
 
-        // wait change quiz when we have a right answer, for not finished quiz with double click wright button
-        if (this.state.answerState) {
-            const key = Object.keys(this.state.answerState)[0];
-            if (this.state.answerState[key] === 'success') {
-                return;
-            }
-        }
+    const results = this.state.rezults;
 
-        console.log('you select:', answerId);
+    if (qwestion.wrightAnswerId === answerId) {
+      if (!results[answerId]) {
+        results[answerId] = "success";
+      }
 
-        const qwestion = this.state.quiz[this.state.activQuestion];
+      // this.state.answerState = {[answerId]: 'success'}
+      this.setState({
+        answerState: { [answerId]: "success" },
+        results: results,
+      });
 
-        const results = this.state.rezults;
-
-        if (qwestion.wrightAnswerId === answerId) {
-
-            if (!results[answerId]) {
-                results[answerId] = 'success';
-            }
-
-            // this.state.answerState = {[answerId]: 'success'}
-            this.setState({
-                answerState: {[answerId]: 'success'},
-                results: results
-            });
-
-            const timeOut = window.setTimeout(() => {
-
-                if (this.quizFinished()) {
-                    this.setState({quizFinished: true});
-                } else {
-                    console.log('you right, next question');
-                    this.chengToNextQuestion();
-                }
-
-                window.clearTimeout(timeOut);
-            }, 1000)
-
-
+      const timeOut = window.setTimeout(() => {
+        if (this.quizFinished()) {
+          this.setState({
+            quizFinished: true,
+            activeHeader: "Eazy-peazy",
+            startFinishedHeader: "You answered all question's",
+          });
         } else {
-            // this.state.answerState = {[answerId]: 'error'}
-            results[answerId] = 'error';
-            this.setState({answerState: {[answerId]: 'error'}, results: results}
-            )
-            console.log('you wrong, think about it....')
-
+          console.log("you right, next question");
+          this.chengToNextQuestion();
         }
 
+        window.clearTimeout(timeOut);
+      }, 1000);
+    } else {
+      // this.state.answerState = {[answerId]: 'error'}
+      results[answerId] = "error";
+      this.setState({ answerState: { [answerId]: "error" }, results: results });
+      console.log("you wrong, think about it....");
     }
+  };
 
+  render() {
+    return (
+      <div className={classes.Quiz}>
+        <div className={classes.QuizWrapper}>
+          <h1>{this.state.activeHeader}</h1>
+          <h2>{this.state.startFinishedHeader}</h2>
 
-    render() {
-        return (
-            <div className={classes.Quiz}>
-                <div className={classes.QuizWrapper}>
-                    <h2>Answer all questions</h2>
-
-                    {
-                        this.state.quizFinished === true ?
-                            <FinishedQuiz
-                                results={this.state.rezults}
-                                quiz={this.state.quiz}
-                            /> :
-                            <ActivQuiz
-                                questionId={this.state.quiz[this.state.activQuestion].questionId}
-                                question={this.state.quiz[this.state.activQuestion].question}
-                                answers={this.state.quiz[this.state.activQuestion].answers}
-                                numberOfQuiz={this.state.quiz.length}
-                                selectQuizAnswer={this.selectQuizAnswerHandler}
-                                answerState={this.state.answerState}
-                            />
-                    }
-
-                </div>
-            </div>
-        )
-    }
+          {this.state.quizFinished === true ? (
+            <FinishedQuiz results={this.state.rezults} quiz={this.state.quiz} />
+          ) : (
+            <ActivQuiz
+              questionId={this.state.quiz[this.state.activQuestion].questionId}
+              question={this.state.quiz[this.state.activQuestion].question}
+              answers={this.state.quiz[this.state.activQuestion].answers}
+              numberOfQuiz={this.state.quiz.length}
+              selectQuizAnswer={this.selectQuizAnswerHandler}
+              answerState={this.state.answerState}
+            />
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Quiz;
